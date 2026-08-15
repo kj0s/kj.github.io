469c1d863b9d47433e96ed03ac8ef222f37cda39
KJ <ksheerjas@gmail.com>
2026-08-15 16:14:19 +1000

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,42 @@ document.addEventListener("mousemove", (event) => {
         `translate(${x * 12}px, ${y * 12}px) rotate(4deg)`;
 
 });
+
+const floatingThings = document.querySelectorAll(
+    ".embroidery-main, .fabric-label"
+);
+
+document.addEventListener("mousemove", (event) => {
+
+    const mouseX =
+        event.clientX / window.innerWidth - 0.5;
+
+    const mouseY =
+        event.clientY / window.innerHeight - 0.5;
+
+
+    floatingThings.forEach((thing, index) => {
+
+        const strength =
+            index === 0 ? 8 : 3;
+
+        const x =
+            mouseX * strength;
+
+        const y =
+            mouseY * strength;
+
+
+        thing.style.setProperty(
+            "--mouse-x",
+            `${x}px`
+        );
+
+        thing.style.setProperty(
+            "--mouse-y",
+            `${y}px`
+        );
+
+    });
+
+});
